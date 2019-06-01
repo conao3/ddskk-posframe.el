a432fb6623d603ccc4baefae30eaf943be98df22
conao3 <conao3@gmail.com>
2019-06-02 02:51:26 +0900

    tagged v1.0.1

--- ddskk-posframe.el
+++ ddskk-posframe.el
@@ -5,7 +5,7 @@
 ;; Author: Naoya Yamashita <conao3@gmail.com>
 ;; Maintainer: Naoya Yamashita <conao3@gmail.com>
 ;; Keywords: convenience
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; URL: https://github.com/conao3/ddskk-posframe.el
 ;; Package-Requires: ((emacs "26.1") (posframe "0.4.3") (ddskk "16.2.50"))
 
